e08240d00b8320783bc47240e8121d08b4bfc68b
Lukas Bischof <lukas.bischof@renuo.ch>
2019-09-26 11:19:07 +0200

    Add commands

--- lib/micro_deploy/cli.rb
+++ lib/micro_deploy/cli.rb
@@ -11,13 +11,20 @@ module MicroDeploy
       puts "I am #{service} #{environment}"
     end
 
-    desc 'down SERVICE [environment]', 'Starts a new webservice'
+    desc 'down SERVICE [environment]', 'Stops a new webservice'
     long_desc <<-LONGDESC.chomp
-      `micro_deploy down [service] [environment]` Start a new webservice called `service` using the specified environment
-      The environment is inferred from it's folder location
+      `micro_deploy down [service] [environment]` Stops a new webservice called `service` using the specified environment
     LONGDESC
     def down(service, environment='master')
-      puts "I am #{service} #{environment}"
+      puts "I am drowning #{service} #{environment}"
+    end
+
+    desc 'restart SERVICE [environment]', 'Restarts a new webservice'
+    long_desc <<-LONGDESC.chomp
+      `micro_deploy restart [service] [environment]` Restarts a new webservice called `service` using the specified environment
+    LONGDESC
+    def restart(service, environment='master')
+      puts "I am drowning and rescuing again #{service} #{environment}"
     end
   end
 end
