b23e00107f6c420c451f6d14c9bec3bbf4b6977a
Lukas Bischof <me@luk4s.dev>
2019-10-17 15:23:51 +0200

    Add git check

--- lib/micro_deploy/cli/project_generate_group.rb
+++ lib/micro_deploy/cli/project_generate_group.rb
@@ -11,8 +11,21 @@ module MicroDeploy
 
       argument :name, type: :string, desc: 'The name of the project'
 
+      def check_if_git_is_installed
+        unless git_installed?
+          $stderr.puts 'Git is not installed!'
+          exit 1
+        end
+      end
+
       def setup_git
         system! "git init --bare #{REPOSITORY_DIRECTORY}", "Initialize empty git repository at #{REPOSITORY_DIRECTORY}"
+      end
+
+      private
+
+      def git_installed?
+        system 'git --version'
       end
     end
   end
